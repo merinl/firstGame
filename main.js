4fddee3c4319641b914694c45cebc672acdd874f
merinl <160482813+merinl@users.noreply.github.com>
2024-04-07 11:14:26 +0530

    added a var for random pick to be used as computer's choice

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 // output to enter player's choice
 let userInput = prompt("Let's play rock, paper, scissor; ------ enter your choice:", "");
+
+
+// create a var that stores random output from 0 - 0.99
+let randomOutput = Math.random();
+
+// create computer choice function
+function getComputerChoice(){
+
+}
